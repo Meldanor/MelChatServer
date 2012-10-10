32463de9a97639342f574b911cc9905b9dcd8c1e
Meldanor <Bhaals@gmx.de>
2012-10-10 22:58:37 +0200

    Forget adding new files
    Signed-off-by: Meldanor <Bhaals@gmx.de>

---
+++ src/main/java/de/meldanor/melchat/server/LoginThread.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2012 Kilian Gärtner
+ * 
+ * This file is part of MelChat.
+ * 
+ * MelChat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * MelChat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MelChat.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.meldanor.melchat.server;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class LoginThread implements Runnable {
+
+    private ServerSocket socket;
+    private ChatServer chatServer;
+
+    public LoginThread(ChatServer chatServer, ServerSocket socket) {
+        this.chatServer = chatServer;
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        Socket client = null;
+        while (true) {
+            try {
+                client = socket.accept();
+                chatServer.addClient(client);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ src/main/java/de/meldanor/melchat/server/PingThread.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2012 Kilian Gärtner
+ * 
+ * This file is part of MelChat.
+ * 
+ * MelChat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * MelChat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MelChat.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.meldanor.melchat.server;
+
+import java.net.Socket;
+import java.util.List;
+import java.util.TimerTask;
+
+public class PingThread extends TimerTask {
+
+    /**
+     * Max Timeout is 5 seconds
+     */
+    private final static int TIMEOUT = 5000;
+
+    private ChatServer chatServer;
+
+    public PingThread(ChatServer chatServer) {
+        this.chatServer = chatServer;
+    }
+
+    @Override
+    public void run() {
+        List<Socket> clients = chatServer.getClients();
+        try {
+            for (Socket client : clients) {
+                if (client.isClosed() || !client.getInetAddress().isReachable(TIMEOUT))
+                    chatServer.closeClient(client);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
